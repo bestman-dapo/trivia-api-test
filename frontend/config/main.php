ef2581d6e1bf89407be4755c7ad9dcb0a208b29c
bestman-dapo <oyebanjioladapo1@gmail.com>
2022-01-06 10:00:57 +0100

    .

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -42,7 +42,7 @@ return [
             'showScriptName' => false,
             'rules' => [
                 ['class' => 'yii\rest\UrlRule', 'controller' => 'score'],
-                ['class' => 'yii\rest\UrlRule', 'controller' => ['getquiz' => 'client']],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'client'],
                 ['class' => 'yii\rest\UrlRule', 'controller' => ['userdata' => 'user']],
                 ['class' => 'yii\rest\UrlRule', 'controller' => 'candidate'],
                 
